9e3239b612ca3e7391ddb318af8d4bfe58329caf
enjia <enjia.wu@gmail.com>
2024-04-28 23:08:59 +0800

    updated practical 3 - 1

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/ListActivity.java
@@ -0,0 +1,64 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+import sg.edu.np.mad.madpractical2.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Profile");
+        builder.setMessage("MADness");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton("VIEW", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id){
+                Intent goToMainActivity = new Intent (ListActivity.this, MainActivity.class);
+                Random random = new Random();
+                goToMainActivity.putExtra("randomNumber",random.nextInt(1-100000));
+                startActivity(goToMainActivity);
+            }
+        });
+        builder.setNegativeButton("CLOSE", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        }
+        );
+
+        ImageView profile = findViewById(R.id.profileIcon);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        }
+        );
+    }
+}
